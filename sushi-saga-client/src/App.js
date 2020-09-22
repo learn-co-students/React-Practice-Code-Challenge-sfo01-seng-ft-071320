c547f65ccc827d11415fafd8d6eeee097a2682c0
Taci Shlosberg <tshlosberg@gmail.com>
2020-09-22 13:52:06 -0700

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,48 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
-  render() {
+  state = {
+    sushis: [],
+    index: 0,
+    cash: 100,
+  }
+  
+  componentDidMount() {
+    fetch(API)
+      .then(resp => resp.json())
+      .then(sushis => 
+        this.setState({
+          sushis: sushis
+        })
+      )
+  }
+
+  showFourSushis = () => {
+    return this.state.sushis.slice(this.state.index, this.state.index + 4)
+  }
+
+  onMoreSushi = () => {
+    this.setState({ 
+      index: this.state.index + 4
+    })
+  }
+
+  onEatenSushi = (sushi) => {
+    const newMoney = this.state.cash - sushi.price;
+    sushi.eaten = true;
+    if (newMoney >= 0) {
+      this.setState({
+        cash: newMoney
+      })
+    }
+  }
+
+
+  render() { 
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushis={this.state.sushis} sushis={this.showFourSushis()} onMoreSushi={this.onMoreSushi} onEatenSushi={this.onEatenSushi}/>
+        <Table cash={this.state.cash} sushi={this.state.sushis.filter(sushi => sushi.eaten)}/>
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,20 +1,16 @@
 import React, { Fragment } from 'react'
 
 const Sushi = (props) => {
+  const eaten = props.sushi.eaten;
+  
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          false ?
-            null
-          :
-            <img src={/* Give me an image source! */ } width="100%" />
-        }
+           onClick={() => {props.onEatenSushi(props.sushi)}}>
+        { eaten ? null : <img src={props.sushi.img_url} width="100%" /> }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {props.sushi.name} - ${props.sushi.price}
       </h4>
     </div>
   )
